80d8ca766572237a83e7a52a6e87b1e97a3f7e00
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-10-04 19:17:46 +0530

    Delete Array_Problems directory

--- Array_Problems/Problem1.java
+++
@@ -1,9 +0,0 @@
-package Array_Problems;
-
-public class Problem1 {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
-
-        for(int i : arr) System.out.print(i + " ");
-    }
-}
